e6d614e3493c38c028f74218d6f8f551eb1f3e6c
Gabriel MARINO <gabriel.marino.csf@gmail.com>
2017-05-30 21:19:56 +0200

    Basic Nav between two views

---
+++ NavigationLearnt/SecondPage.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+} from 'react-native';
+
+export default class SecondPage extends Component {
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Le code complet de cette view est partagé ios/android
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ NavigationLearnt/index.ios.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+import React, { AppRegistry } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import HomePage from './HomeComponent.js';
+import SecondPage from './SecondPage.js';
+
+const NavigationLearnt = StackNavigator({
+   Home : { screen: HomePage },
+   SecondPage : {screen : SecondPage},
+});
+
+AppRegistry.registerComponent('NavigationLearnt', () => NavigationLearnt);
