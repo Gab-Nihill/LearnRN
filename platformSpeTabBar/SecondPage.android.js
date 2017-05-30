14a46477c9434f2d8e2036baa1c59878026fcdec
Gabriel MARINO <gabriel.marino.csf@gmail.com>
2017-05-31 00:33:49 +0200

    [WIP] Example with platform specific tabbar component and common child views (android tabBar in progress)

---
+++ platformSpeTabBar/SecondPage.android.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+} from 'react-native';
+
+export default class SecondPage extends Component {
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Cette vue est spécifique à la plateforme Android
+        </Text>
+        <Text style={styles.instructions}>
+          Grâce aux sub-extension .ios & .android React Native permet de différencier un composant selon la plateforme (ex : index.ios.js) {"\n"}
+          Il est aussi possible de modifier certains morceaux d'un composant via le module React Native Platform
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
