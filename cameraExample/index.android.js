8b132df433f9ec4b1945e2b1f7ed0f0c8b3a8472
Gabriel MARINO <gabriel.marino.csf@gmail.com>
2017-06-01 23:12:36 +0200

    Fixed Camera Example for android

--- cameraExample/index.android.js
+++ cameraExample/index.android.js
@@ -3,6 +3,10 @@
  * https://github.com/facebook/react-native
  * @flow
  */
+ import React, { AppRegistry } from 'react-native';
+ import { StackNavigator } from 'react-navigation';
+ import HomePage from './HomeComponent';
+ import SecondPage from './SecondPage';
 
  const cameraExample = StackNavigator({
     Home : { screen: HomePage },
